823ab429880cc4f3ce2936445bd5fb19ae0cb859
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-14 14:43:43 -0500

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public double Deposite(double dep)
+        {
+            return balance += dep;
+        }
+
+        public double Withdraw(double dep)
+        {
+            return balance -= dep;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿using BankEncapsulation;
+
+var account1 = new BankAccount();
+
+account1.Deposite(121);
+account1.Deposite(165);
+account1.Deposite(78);
+
+Console.WriteLine(account1.GetBalance()); 
